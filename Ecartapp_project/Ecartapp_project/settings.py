884f813f2318aaedfbb480405382da0b3889c291
Anku96 <satikosre.ankit@gmail.com>
2024-10-09 22:49:37 +0530

    my changes

--- Ecartapp_project/Ecartapp_project/settings.py
+++ Ecartapp_project/Ecartapp_project/settings.py
@@ -32,6 +32,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'orders',
+    'store',
+    'category',
     'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
